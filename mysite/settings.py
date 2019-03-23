fa765efe3eed3a4b6b52a860ef2916ce995c6123
Samantha Doctora <samanthakate4@yahoo.co.nz>
2019-03-23 15:53:36 +1100

    Fixed ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v#do*&r6xv3^-uv9ptwg3y!8xj8zf_cbvcvc+n#o97@fhdkcy='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -120,7 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
 DATABASES = {
     'default': {
